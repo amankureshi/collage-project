b213cc5d1aa550afc53e2c3f7879681b94fd82b7
Aman Kureshi <amankureshi78692@gmail.com>
2025-02-11 23:05:14 +0530

    added dynamic navbar and footer and nav-link hover bg-color changed

---
+++ footerCommon.js
@@ -0,0 +1,38 @@
+document.getElementById("footer").innerHTML = `
+   <footer class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 mb-md-0 mb-4">
+                    <h5>About us</h5>
+                    <p class="">
+                        Armanee Tea is a Premium Tea Company, having presence in tea
+                        business since 2010.Armanee Tea , blends natural teas, along
+                        with organic teas. The Armanee Tea enriches people with their
+                        various products of tea.
+                    </p>
+
+                    <a href="#" class="btn btn-sm btn-warning">View more</a>
+                </div>
+                <div class="col-md-1"></div>
+                <div class="col-md-3 mb-md-0 mb-4">
+                    <h5>Useful Links</h5>
+                    <ul>
+                        <li><a href="/">Home</a></li>
+                        <li><a href="about">About us</a></li>
+                        <li><a href="shop">Shop</a></li>
+                        <li><a href="contact">Contact us</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-4">
+                    <h5>Address</h5>
+                    <p><i class="fa-regular fa-envelope"></i> armaneetea@gmail.com</p>
+                    <p><i class="fa-solid fa-phone"></i> (+91)-97128 27603</p>
+                    <p>
+                        <i class="fa-solid fa-location-dot"></i>
+                        Unava Bypass Highway, At. &, Dist. Agol,(North Gujarat) INDIA
+                    </p>
+                </div>
+            </div>
+        </div>
+    </footer>
+`;
---
+++ navbar.js
@@ -0,0 +1,30 @@
+document.getElementById("navbar").innerHTML = `
+   <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="#"><img src="images/Tea_logo.png" class="logo" alt=""></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse " id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto ">
+                    <li class="nav-item">
+                        <a class="nav-link text-dark" aria-current="page" href="index.html">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-dark" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-dark" href="#">Shop</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-dark" href="#">Contact</a>
+                    </li>
+
+                    <button class="btn btn-theme text-white rounded-pill px-4">My account</button>
+                </ul>
+            </div>
+        </div>
+    </nav>
+`;
