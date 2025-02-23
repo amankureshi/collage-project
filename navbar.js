cf6a4c0de978824b6252743e854861ebd3b77ecf
Aman Kureshi <amankureshi78692@gmail.com>
2025-02-23 17:56:33 +0530

    added tea-product class and new tea product image in shop component

--- navbar.js
+++ navbar.js
@@ -8,7 +8,7 @@ document.getElementById("navbar").innerHTML = `
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse " id="navbarSupportedContent">
-                <ul class="navbar-nav ms-auto ">
+                <ul class="navbar-nav ms-auto gap-2 ">
                     <li class="nav-item">
                         <a class="nav-link text-secondary" aria-current="page" href="index.html">Home</a>
                     </li>
