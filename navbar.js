6ca15ac9591d9daf36a148795aa982d4d8419418
Aman Kureshi <amankureshi78692@gmail.com>
2025-02-26 21:41:54 +0530

    added new navbar toggle

--- navbar.js
+++ navbar.js
@@ -11,7 +11,7 @@ document.getElementById("navbar").innerHTML = `
             <div class="collapse navbar-collapse " id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto gap-2">
                     <li class="nav-item">
-                        <a class="nav-link text-dark" aria-current="page" href="#">Home</a>
+                        <a class="nav-link text-dark" aria-current="page" href="index.html">Home</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link text-dark" href="about.html">About</a>
